51df0fa6298c50684d0c98bb2cde88d299887486
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-08-25 09:36:56 +0530

    great

--- src/components/ScrollContent.tsx
+++ src/components/ScrollContent.tsx
@@ -1,75 +1,25 @@
 "use client";
-import React from 'react'
+import React from "react";
 import { StickyScroll } from "./ui/sticky-scroll-reveal";
 import Image from "next/image";
 
-
-// const content = [
-//   {
-//     title: "Responsive Design",
-//     description:
-//       "Crafting layouts that adapt seamlessly to any device, ensuring a consistent and enjoyable user experience.",
-//       content: (
-//         <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-//           Collaborative Editing
-//         </div>
-//       ),
-//   },
-//   {
-//     title: "Interactive UI/UX",
-//     description:
-//       "Implementing intuitive and engaging interfaces that enhance user interaction and satisfaction.",
-//       content: (
-//         <div className="h-full w-full  flex items-center justify-center text-white">
-//           <Image
-//             src="/linear.webp"
-//             width={300}
-//             height={300}
-//             className="h-full w-full object-cover"
-//             alt="linear board demo"
-//           />
-//         </div>
-//       ),
-//   },
-//   {
-//     title: "Performance Optimization",
-//     description:
-//       "Focusing on fast load times and smooth performance to keep users engaged and content.",
-//       content: (
-//         <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
-//           Version control
-//         </div>
-//       ),
-//   },
-//   {
-//     title: "Accessibility",
-//     description:
-//       "Building accessible websites that cater to all users, including those with disabilities, following best practices and guidelines",
-//       content: (
-//         <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
-//           Running out of content
-//         </div>
-//       ),
-//   },
-// ];
-
 const content = [
   {
-    title: "Collaborative Editing",
+    title: "Responsive Design",
     description:
-      "Work together in real time with your team, clients, and stakeholders. Collaborate on documents, share ideas, and make decisions quickly. With our platform, you can streamline your workflow and increase productivity.",
+      "Crafting layouts that adapt seamlessly to any device, ensuring a consistent and enjoyable user experience.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+      <div className="h-full w-full bg-gradient-to-br from-cyan-500 to-emerald-500 flex items-center justify-center text-white">
         Collaborative Editing
       </div>
     ),
   },
   {
-    title: "Real time changes",
+    title: "Interactive UI/UX",
     description:
-      "See changes as they happen. With our platform, you can track every modification in real time. No more confusion about the latest version of your project. Say goodbye to the chaos of version control and embrace the simplicity of real-time updates.",
+      "Implementing intuitive and engaging interfaces that enhance user interaction and satisfaction.",
     content: (
-      <div className="h-full w-full  flex items-center justify-center text-white">
+      <div className="h-full w-full flex items-center justify-center text-white">
         <Image
           src="/linear.webp"
           width={300}
@@ -81,21 +31,21 @@ const content = [
     ),
   },
   {
-    title: "Version control",
+    title: "Performance Optimization",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "Focusing on fast load times and smooth performance to keep users engaged and content.",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--orange-500),var(--yellow-500))] flex items-center justify-center text-white">
+      <div className="h-full w-full bg-gradient-to-br from-orange-500 to-yellow-500 flex items-center justify-center text-white">
         Version control
       </div>
     ),
   },
   {
-    title: "Running out of content",
+    title: "Accessibility",
     description:
-      "Experience real-time updates and never stress about version control again. Our platform ensures that you're always working on the most recent version of your project, eliminating the need for constant manual updates. Stay in the loop, keep your team aligned, and maintain the flow of your work without any interruptions.",
+      "Building accessible websites that cater to all users, including those with disabilities, following best practices and guidelines",
     content: (
-      <div className="h-full w-full bg-[linear-gradient(to_bottom_right,var(--cyan-500),var(--emerald-500))] flex items-center justify-center text-white">
+      <div className="h-full w-full bg-gradient-to-br from-cyan-500 to-emerald-500 flex items-center justify-center text-white">
         Running out of content
       </div>
     ),
@@ -107,14 +57,14 @@ function ScrollContent() {
     <div>
       <div className="p-10">
         <div className="pt-10 text-center">
-          <p className='text-3xl sm:text-4xl font-bold leading-8 tracking-tight mt-2 text-white'>
-            WHAT I DO ?
+          <p className="text-3xl sm:text-4xl font-bold leading-8 tracking-tight mt-2 text-white">
+            WHAT I DO?
           </p>
         </div>
         <StickyScroll content={content} />
       </div>
     </div>
-  )
+  );
 }
 
-export default ScrollContent
+export default ScrollContent;
