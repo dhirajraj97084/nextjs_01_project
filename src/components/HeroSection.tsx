7498d0bab1b5d6b06fd630e9ffd70393ea7de511
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-08-25 08:48:34 +0530

    nice

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -14,7 +14,7 @@ function HeroSection() {
           fill="white"
         />
         <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
-          <h1 className="text-4xl md:text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          <h1 className="text-3xl md:text-5xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
            HII , I'M DHIRAJ KUMER TIWARI.
           </h1>
           <p className="mt-4 font-normal text-base text-neutral-300 max-w-3xl text-center mx-auto">
