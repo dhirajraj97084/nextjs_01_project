37cd8baefdff8c9b66fc1fb1364ca0210c9863ae
Dhiraj-Tiwari <rajtiwari73011@gmail.com>
2024-08-26 07:59:22 +0530

    best

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -8,7 +8,7 @@ import { Button } from "./ui/moving-border";
 function HeroSection() {
   return (
     <div>
-      <div className=" w-full md:h-[42rem] h-[35rem] rounded-md flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      {/* <div className=" w-full md:h-[42rem] h-[35rem] rounded-md flex items-center justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
         <Spotlight
           className=" left-0 md:left-60 -top-20"
           fill="white"
@@ -18,7 +18,7 @@ function HeroSection() {
            HII , I'M DHIRAJ KUMER TIWARI.
           </h1>
           <p className="mt-4 font-normal text-base text-neutral-300 max-w-3xl text-center mx-auto">
-          Welcome to my portfolio! I specialize in creating visually appealing, responsive, and user-friendly websites that deliver exceptional user experiences. With a passion for coding and a keen eye for design, I turn ideas into interactive, accessible,&apos; and engaging&apos; web applications.
+          Welcome to my portfolio! I specialize in creating visually appealing, responsive, and user-friendly websites that deliver exceptional user experiences. With a passion for coding and a keen eye for design, I turn ideas into interactive, accessible, and engaging web applications.
           </p>
           <div className=' flex justify-center items-center pt-5 md:pt-10'>
             <Link href={'/explore'}>
@@ -34,7 +34,25 @@ function HeroSection() {
             </Link>
           </div>
         </div>
+      </div> */}
+
+<div className="h-[40rem] w-full rounded-md flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+      <Spotlight
+        className="-top-40 left-0 md:left-60 md:-top-20"
+        fill="white"
+      />
+      <div className=" p-4 max-w-7xl  mx-auto relative z-10  w-full pt-20 md:pt-0">
+        <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+          Spotlight <br /> is the new trend.
+        </h1>
+        <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+          Spotlight effect is a great way to draw attention to a specific part
+          of the page. Here, we are drawing the attention towards the text
+          section of the page. I don&apos;t know why but I&apos;m running out of
+          copy.
+        </p>
       </div>
+    </div>
     </div>
   )
 }
